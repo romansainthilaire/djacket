bc0e6ef523af4151ad3c36be7f1d1044fa65b93a
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2025-10-23 07:22:06 +0200

    base vue project

---
+++ djacket/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router"
+import HelloWorld from "./components/HelloWorld.vue"
+</script>
+
+<template>
+
+  <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" />
+
+  <HelloWorld />
+
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
