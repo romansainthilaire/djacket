16b2739fbc92fd5bc64d39a071d854ec420d851e
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2025-11-21 01:21:44 +0100

    VerifyEmailNoticeView - clean

--- djacket/src/views/VerifyEmailNoticeView.vue
+++ djacket/src/views/VerifyEmailNoticeView.vue
@@ -5,12 +5,10 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div>
-    <h1>Vérification requise</h1>
-    <p>Un e-mail de vérification a été envoyé à : <b>{{ props.email }}</b></p>
-    <p>Cliquez sur le lien reçu pour activer votre compte (valide 24h).</p>
-    <p>👉 Pensez à vérifier votre dossier spam.</p>
-  </div>
+  <h1>Vérification requise</h1>
+  <p>Un e-mail de vérification a été envoyé à : <b>{{ props.email }}</b></p>
+  <p>Cliquez sur le lien reçu pour activer votre compte (valide 24h).</p>
+  <p>👉 Pensez à vérifier votre dossier spam.</p>
 </template>
 
 <style scoped>
